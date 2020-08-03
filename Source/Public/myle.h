be0064017b4f16f59e767eb3dd135ab92a090c87
Maximilian Burger <mabug@outlook.de>
2020-08-03 10:51:42 +0200

    Update myle.h

--- Source/Public/myle.h
+++ Source/Public/myle.h
@@ -13,6 +13,9 @@ class MYLE
 public:
     MYLE();
 
+    namespace AdvancedStringLibrary
+    {
+    
     ///
     /// \brief randomString Returns a std::string with randomly placed characters based on your charset with the given length
     /// \param length Length of the random string
@@ -28,6 +31,14 @@ public:
     ///
     std::string randomString(int length);
 
+    void replaceAll(const int& from, const int& to, const char& toFind, const char& replaceWith, std::string &__string);
+
+
+    } // namespace AdvancedStringLibrary
+
+    namespace AdvancedNumberLibrary
+    {
+    
     ///
     /// \brief randomIntegerInRange Generates a random integer in the given bounds
     /// \param from Startindex
@@ -36,6 +47,18 @@ public:
     ///
     int randomIntegerInRange(int from, int to);
 
+    ///
+    /// \brief IntegerToBinaryString converts a number into a binary string in the format %32s
+    /// \param number number to convert
+    /// \return std::string binary string
+    ///
+    std::string IntegerToBinaryString(int& number);
+
+    } // namespace AdvancedNumberLibrary
+
+    namespace FileIO
+    {
+        
     ///
     /// \brief writeLineToFile Write a single string to a file fast
     /// \param path Path
@@ -57,16 +80,24 @@ public:
     ///
     std::vector<std::string> readFile(std::string &path);
 
-    std::string IntegerToBinaryString(int& number);
+    } // namespace FileIO
 
-    void replaceAll(const int& from, const int& to, const char& toFind, const char& replaceWith, std::string &__string);
-
+    namespace Algorithms
+    {
+        
+    // TODO
     int luhn_checksum(std::string number);
 
+    // TODO
     bool luhn(std::string number);
 
 private:
+
+    //TODO
     int getLuhnSum(std::string number);
+
+    } // namespace Algorithms
+
 };
 
 #endif // MYLE_H
