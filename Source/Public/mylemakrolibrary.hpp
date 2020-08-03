eb99905131275154f64084a27d199357054abe01
Maximilian Burger <mabug@outlook.de>
2020-08-03 13:41:21 +0200

    Update mylemakrolibrary.hpp

--- Source/Public/mylemakrolibrary.hpp
+++ Source/Public/mylemakrolibrary.hpp
@@ -6,4 +6,13 @@
 ///
 #define FWD(...) ::std::forward<decltype(__VA_ARGS__)>(__VA_ARGS__)
 
+///
+/// \brief You can use this if an exception is not thrown under a certaint condition/expression
+///
 #define noexcept_for(expression) noexcept(noexcept(expression))
+
+
+///
+/// \brief You can you use AGAIN in your fori loop to skip the current iteration and redo it in the same index
+///
+#define AGAIN(index) index--; continue;
